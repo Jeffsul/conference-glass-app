70012056f0b38e97049856cb04c8f4d3e90d0065
Jeffsul <jeffsul16@gmail.com>
2015-01-28 10:15:55 -0500

    Updated User model

--- GlassConference/app/src/main/java/com/syde461/group6/glassconference/User.java
+++ GlassConference/app/src/main/java/com/syde461/group6/glassconference/User.java
@@ -7,19 +7,31 @@ public class User {
     private static final String DEFAULT_IMAGE = "";
 
     private final String name;
+    private final String employer;
+    private final String position;
     private final String image;
 
     public User(String name) {
-        this(name, DEFAULT_IMAGE);
+        this(name, "", "", DEFAULT_IMAGE);
     }
 
-    public User(String name, String image) {
+    public User(String name, String employer, String position, String image) {
         this.name = name;
+        this.employer = employer;
+        this.position = position;
         this.image = image;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public String getPosition() {
+        return position;
     }
 
     public String getImage() {
